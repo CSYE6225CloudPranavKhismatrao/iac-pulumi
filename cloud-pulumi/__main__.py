26962a8d2965c89429652ec5eb8813f28de07b91
Pranav014 <pranav.k1998@gmail.com>
2023-10-12 16:53:51 -0400

    ff

---
+++ cloud-pulumi/__main__.py
@@ -0,0 +1,130 @@
+import pulumi
+import pulumi_aws as aws
+import ipaddress
+
+
+
+ 
+
+# Fetch the configuration values
+config = pulumi.Config()
+data = config.require_object("data")
+
+
+# Extract key configuration values
+vpc_name = data.get("vpcName")
+vpc_cidr = data.get("vpcCidr")
+
+def generate_ip_addresses(subnet):
+    try:
+        network = ipaddress.IPv4Network(subnet, strict=False)
+        ip_list = [str(ip) for ip in network.hosts()]
+        return ip_list
+    except ValueError as e:
+        return str(e)
+
+# subnet = "10.0.1.0/24"
+ip_addresses = generate_ip_addresses(data.get("vpcCidr"))
+
+# Create the VPC using the fetched config values
+Virtual_private_cloud = aws.ec2.Vpc(vpc_name,
+    cidr_block=vpc_cidr,
+    instance_tenancy="default",
+    tags={
+        "Name": vpc_name,
+    })
+
+ 
+
+# Define availability zones
+azs = aws.get_availability_zones().names
+num_azs = len(azs)
+
+no_of_subnets = 3 # max
+
+print(num_azs)
+
+if (num_azs < 3):
+    no_of_subnets = num_azs * 2
+# Create 3 public and 3 private subnets
+public_subnets = []
+private_subnets = []
+
+ 
+k = 0
+for i in range(no_of_subnets):
+    az_index = i % num_azs
+    public_subnet = aws.ec2.Subnet(f"{vpc_name}-public-subnet-{i}",
+        cidr_block= data.get(f"publicSubnetCidr{i}"),
+        availability_zone=azs[az_index],
+        vpc_id=Virtual_private_cloud.id,
+        map_public_ip_on_launch=True,
+        tags={
+            "Name": f"{vpc_name}-public-subnet-{i}",
+        })
+
+    k += 1
+
+    private_subnet = aws.ec2.Subnet(f"{vpc_name}-private-subnet-{i}",
+        cidr_block= data.get(f"privateSubnetCidr{i}"),
+        availability_zone=azs[az_index],
+        vpc_id=Virtual_private_cloud.id,
+        tags={
+            "Name": f"{vpc_name}-private-subnet-{i}",
+        })
+    k += 1
+ 
+
+    public_subnets.append(public_subnet)
+    private_subnets.append(private_subnet)
+
+ 
+
+# Create an Internet Gateway and attach it to the VPC
+internet_gateway = aws.ec2.InternetGateway(f"{vpc_name}-internet-gateway",
+    vpc_id=Virtual_private_cloud.id,
+    tags={
+        "Name": f"{vpc_name}-internet-gateway",
+    })
+
+ 
+
+# Create a public route table
+public_route_table = aws.ec2.RouteTable(f"{vpc_name}-public-route-table",
+    vpc_id=Virtual_private_cloud.id,
+    tags={
+        "Name": f"{vpc_name}-public-route-table",
+    })
+
+ 
+
+# Associate public subnets with the public route table
+for subnet in public_subnets:
+    aws.ec2.RouteTableAssociation(f"{subnet._name}-association",
+        subnet_id=subnet.id,
+        route_table_id=public_route_table.id)
+
+ 
+
+# Create a private route table
+private_route_table = aws.ec2.RouteTable(f"{vpc_name}-private-route-table",
+    vpc_id=Virtual_private_cloud.id,
+    tags={
+        "Name": f"{vpc_name}-private-route-table",
+    })
+
+ 
+
+# Associate private subnets with the private route table
+for subnet in private_subnets:
+    aws.ec2.RouteTableAssociation(f"{subnet._name}-association",
+        subnet_id=subnet.id,
+        route_table_id=private_route_table.id)
+
+ 
+
+# Create a public route in the public route table
+public_route = aws.ec2.Route(f"{vpc_name}-public-route",
+    route_table_id=public_route_table.id,
+    destination_cidr_block="0.0.0.0/0",
+    gateway_id=internet_gateway.id)
